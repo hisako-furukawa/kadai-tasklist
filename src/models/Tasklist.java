bf93c45e0a968a42cec2b6f9c81197345e6f9af0
hisako.furukawa <hisakogazimaru@gmail.com>
2020-08-03 20:05:48 +0900

    Add getAlltask

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -7,9 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAlltask",
+            query = "SELECT m FROM Tasklist AS m ORDER BY m.id DESC"
+            )
+})
+
 @Table(name = "tasks")
 
 public class Tasklist {
@@ -26,5 +36,39 @@ public class Tasklist {
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
 
 }
