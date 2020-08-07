d36de3bc1ec2766bc975bccc29da2c18a387304c
hisako.furukawa <hisakogazimaru@gmail.com>
2020-08-08 01:42:32 +0900

    Add DestroyServlet.java

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Tasklist m = em.find(Tasklist.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);       // データ削除
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("tasklist_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+    }
+    }
+
+}
