a7b9d697d618e1e77a51a2b624d32ddea3270e10
hisako.furukawa <hisakogazimaru@gmail.com>
2020-08-08 01:27:00 +0900

    Add UpdateServlet.java

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Tasklist;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Tasklist m = em.find(Tasklist.class, (Integer)(request.getSession().getAttribute("tasklist_id")));
+
+            // フォームの内容を各プロパティに上書き
+
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("tasklist_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+
+}
